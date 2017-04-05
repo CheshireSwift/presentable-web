4c0b728e64d3b6aa075755663334dc86a84b7ef3
Sam Collard <swift@swifteagle.co.uk>
2017-04-05 14:13:37 +0100

    💔 Refactor API
    Behaviour is now:
    - slideNumber starts at 0 (i.e. first elements to be shown should be `.slide-1`)
    - When slideNumber is X, elements with `.slide-X` will have `.slide-current` applied.
    - When slideNumber is >X, elements with `.slide-X` will have `.slide-previous` applied.
    - advance() will result in no change if there are no elements with `.slide-X` for X = slideNumber + 1
    - retreat() will result in no change if there are no elements with `.slide-X` for X = slideNumber
    
    This allows for various presentation styles; assuming all slide elements have `visibility: hidden` by default, can specify `visibility: visible !important` on just `.slide-current` or on both `.slide-current, .slide-previous`.

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
-//  <script type="text/javascript" src="//rawgit.com/SimonWaldherr/ColorConverter.js/master/colorconverter.min.js"></script>
-//  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/highlight.min.js"></script>
-//  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/dustjs-linkedin/2.7.4/dust-full.min.js"></script>
-//  <link rel="stylesheet" href="//unpkg.com/tachyons@4.5.3/css/tachyons.min.css" />
-//  <link rel="stylesheet" href="//rawgit.com/isagalaev/highlight.js/master/src/styles/monokai-sublime.css">
-//  <style type="text/css">
-//    .adv-hide {
-//      visibility: hidden;
-//    }
-//  </style>
+var slideCounter = 0
 
-//        <a href="javascript:slides.retreat()">◀</a>
-//        <a href="javascript:slides.advance()">▶</a>
-//
-//      <div class="adv-1 adv-hide"> </div>
-//      <div class="adv-2 adv-hide"> </div>
-//      <div class="adv-3 adv-hide"> </div>
-//
-//      <div id="iphone-container" class="adv-0 adv-hide"></div>
-
-
-var advCounter = 0;
+function styleElems(classListUpdate, slideModifier) {
+  var modifier = slideModifier || 0
+  var slideToChange = slideCounter + modifier
+  var elems = document.getElementsByClassName('slide-' + slideToChange)
+  for (var i = 0; i < elems.length; i++) {
+    classListUpdate(elems.item(i).classList)
+  }
+  
+  return !!elems.length
+}
 
 function advance() {
-  var elems = document.getElementsByClassName('adv-' + advCounter)
-  if (!elems.length) {
+  slideCounter++
+  var found = styleElems(classList => classList.add('slide-current'))
+  if (!found) {
+    slideCounter--
     return
   }
-
-  for (var i = 0; i < elems.length; i++) {
-    elems.item(i).classList.remove('adv-hide')
-  }
-  advCounter++
+  
+  styleElems(classList => {
+    classList.remove('slide-current')
+    classList.add('slide-previous')
+  }, -1)
 }
 
 function retreat() {
-  if (advCounter > 0) {
-    advCounter--
+  var elems = document.getElementsByClassName('slide-' + slideCounter)
+  var found = styleElems(classList => classList.remove('slide-current'))
+  if (!found) {
+    return
   }
-
-  var elems = document.getElementsByClassName('adv-' + advCounter)
-  for (var i = 0; i < elems.length; i++) {
-    elems.item(i).classList.add('adv-hide')
-  }
+  
+  slideCounter--
+  styleElems(classList => {
+    classList.remove('slide-previous')
+    classList.add('slide-current')
+  })
 }
 
-document.addEventListener('keydown', e => (e.keyCode == 39) && advance())
-document.addEventListener('keydown', e => (e.keyCode == 37) && retreat())
+function slideNumber() {
+  return slideCounter
+}
+
+function registerForKeys() {
+  // right cursor, page down
+  document.addEventListener('keydown', e => (e.keyCode == 39 || e.keyCode == 34) && advance())
+  
+  // left cursor, page up
+  document.addEventListener('keydown', e => (e.keyCode == 37 || e.keyCode == 33) && retreat())
+}
 
 function registerForClicks() {
   document.addEventListener('contextmenu', e => e.preventDefault())
   document.addEventListener('click', e => e.button ? retreat() : advance())
 }
 
-function slideNumber() {
-  return advCounter
+function configure(opts) {
+  if (opts.keyboard) registerForKeys()
+  if (opts.mouse) registerForClicks()
 }
 
-module.exports = { advance, retreat, slideNumber, registerForClicks }
+module.exports = { advance, retreat, slideNumber, configure }
